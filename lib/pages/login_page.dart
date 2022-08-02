e36c11a5d75f38d2e5d1d71ded6d0ade9364b7b1
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-02 09:14:59 -0300

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:untitled/pages/home_page.dart';
 import 'package:untitled/pages/pacote_telaPrincipal.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,87 +14,109 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController userController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xFFFFE8E8),
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 32),
-              Image.asset(
-                "imagens/logo.png",
-                width: 250,
-                height: 250,
-                fit: BoxFit.fitHeight,
-              ),
-              const SizedBox(height: 32),
-              TextField(
-                controller: userController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'E-mail ou usuário',
-                ),
-                cursorColor: Color(0xFFDD2E44),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Senha',
-                ),
-                cursorColor: Color(0xFFDD2E44),
-              ),
-              const SizedBox(height: 24),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFFFCC99)
-                ),
-                  onPressed: onPressed,
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12.0),
-                    child: Text(
+        child: Scaffold(
+          backgroundColor: const Color(0xFFFFE8E8),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 32),
+                  Image.asset(
+                    "images/logo.png",
+                    width: 250,
+                    height: 250,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  const SizedBox(height: 32),
+                  TextFormField(
+                    controller: userController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Campo e-mail obrigatório';
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'E-mail ou usuário',
+                    ),
+                    cursorColor: Color(0xFFDD2E44),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Campo senha obrigatório';
+                      } else if (value.length < 9) {
+                        return 'A senha deve conter o mínimo de 8 dígitos';
+                      }
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                    cursorColor: Color(0xFFDD2E44),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFFFCC99)
+                    ),
+                    onPressed: onPressed,
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12.0),
+                      child: Text(
                         'Entrar com gitHub',
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFFDD2E44)
+                        style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFFDD2E44)
+                        ),
                       ),
                     ),
-                  ),
-              )
-            ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      )
+        )
     );
   }
 
-  void onPressed(){
-    String userLogin = "AcademicSyllabus2022";
-    String passwordLogin = "ASapp2022";
+  void onPressed() {
+    if (_formKey.currentState!.validate()) {
+      String userLogin = "AcademicSyllabus2022";
+      String passwordLogin = "ASapp2022";
 
-    String user = userController.text;
-    String pwd = passwordController.text;
+      String user = userController.text;
+      String pwd = passwordController.text;
 
-    if(userLogin == user && passwordLogin == pwd){
-
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) {
-                return const TelaPrincipalPage();
-              }
-          )
-      );
-
-    } else{
-      print("Usuário ou senha incorretos. Tente novamente.");
+      if (userLogin == user && passwordLogin == pwd) {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) {
+                  return const TelaPrincipalPage();
+                }
+            )
+        );
+      } else {
+        print("Usuário ou senha incorretos. Tente novamente.");
+      }
+    } else {
+      print("Formulário inválido.");
     }
   }
+
 }
