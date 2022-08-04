fa193b3f872235cb71e06fed4c09c057397a2907
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-04 17:05:15 -0300

    Add files via upload

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFE8E8),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('CONFIGURAÇÕES'),
+        backgroundColor: const Color(0xFFDD2E44),
+      ),
+      body: BuildBody(),
+
+    );
+  }
+
+  BuildBody(){
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  'Perfil de usuário',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Color(0xFFDD2E44),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
