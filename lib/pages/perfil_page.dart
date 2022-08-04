a614999ef2f3f570b8f99c35ef97f1b57d032b36
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-04 17:05:04 -0300

    Update perfil_page.dart

--- lib/pages/perfil_page.dart
+++ lib/pages/perfil_page.dart
@@ -7,44 +7,62 @@ class PerfilPage extends StatefulWidget {
   _PerfilPageState createState() => _PerfilPageState();
 }
 
-
 class _PerfilPageState extends State<PerfilPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFDD2E44),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: ListView(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "images/perfil.png",
-
-                    width: 300,
-                    height: 300,
-                    fit: BoxFit.fitHeight,
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    'Perfil de usuário',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      backgroundColor: const Color(0xFFFFE8E8),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('PERFIL DE USUÁRIO'),
+        backgroundColor: const Color(0xFFDD2E44),
       ),
+      body: BuildBody(),
     );
   }
 
+  BuildBody() {
+    return ListView(
+      children: [
+        Container(
+          height: 300,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/perfilWallpaper.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/perfil.png")
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          "images/perfil.png",
+          width: 200,
+          height: 200,
+          fit: BoxFit.fitHeight,
+        ),
+        const SizedBox(height: 16),
+        const Text(
+          'Nome do usuário',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            color: Color(0xFFDD2E44),
+          ),
+        ),
+      ],
+    );
+  }
 }
