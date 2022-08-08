df05a66ef2208d81f92846ac46c7362a865b585d
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-07 21:24:52 -0300

    Update pacote_tarefas.dart

--- lib/domain/pacote_tarefas.dart
+++ lib/domain/pacote_tarefas.dart
@@ -6,15 +6,20 @@ class PacoteTarefas {
   //final String imagem;
   //final String tarefa;
   final String nomeLista;
-  final String tarefa;
+  final String tarefa1;
+  final String tarefa2;
+  final String tarefa3;
+  final String tarefa4;
+  final String tarefa5;
   final int cor;
 
   PacoteTarefas({
-    //required this.icon,
-    //required this.imagem,
-    //required this.tarefa,
     required this.nomeLista,
-    required this.tarefa,
+    required this.tarefa1,
+    required this.tarefa2,
+    required this.tarefa3,
+    required this.tarefa4,
+    required this.tarefa5,
     required this.cor,
   });
 }
