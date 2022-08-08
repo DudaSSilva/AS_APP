970786e66766c47eaa873d11c2db45d1b2683d23
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-07 21:43:09 -0300

    Create pacote_conquistas.dart

---
+++ lib/domain/pacote_conquistas.dart
@@ -0,0 +1,16 @@
+
+//import 'package:flutter/cupertino.dart';
+
+class PacoteConquistas {
+  final Icon icon;
+  final String title;
+  final String text;
+  final int cor;
+
+  PacoteConquistas({
+    required this.icon,
+    required this.title,
+    required this.text,
+    required this.cor,
+  });
+}
