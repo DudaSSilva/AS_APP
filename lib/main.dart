f7b7810d412b4974008c5a66511e78ab5ed1d44e
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-09 09:42:02 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/pages/splash_screen.dart';
 import 'pages/home_page.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-        home: HomePage()
+        home: Splash()
     ),
   );
 }
