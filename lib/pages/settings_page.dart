54ac2246d87416c995889751a528ba56ad9981a8
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-23 16:35:55 -0300

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -9,8 +9,18 @@ class SettingsPage extends StatefulWidget {
 
 
 class _SettingsPageState extends State<SettingsPage> {
+  //String selectedTheme;
+
   @override
   Widget build(BuildContext context) {
+    // setState((){
+    //   if (Theme.of(context).brightness == Brightness.dark){
+    //     selectedTheme = 'dark';
+    //   } else {
+    //     selectedTheme = 'light';
+    //   }
+    // });
+
     return Scaffold(
       backgroundColor: const Color(0xFFFFE8E8),
       appBar: AppBar(
@@ -40,6 +50,7 @@ class _SettingsPageState extends State<SettingsPage> {
                     color: Colors.black,
                   ),
                 ),
+                // buildCardWidget()
               ],
             ),
           )
@@ -47,4 +58,57 @@ class _SettingsPageState extends State<SettingsPage> {
       )
     );
   }
+
+  buildCardWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Tema',
+          style: TextStyle(fontSize: 24),
+        ),
+        Container(
+          height: 20,
+        ),
+        /*Row(
+          children: [
+            Radio(
+              value: 'light',
+              groupValue: selectedTheme,
+              onChanged: themeSelection,
+            ),
+            const Text(
+              'Modo claro',
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            Radio(
+              value: 'dark',
+              groupValue: selectedTheme,
+              onChanged: themeSelection,
+            ),
+            const Text(
+              'Modo escuro',
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        )*/
+      ],
+    );
+  }
+
+  // void themeSelection(String value) {
+  //   setState((){
+  //     selectedTheme = value;
+  //   });
+  //   if (value == 'light') {
+  //     widget.changeTheme(Brightness.light);
+  //   } else {
+  //     widget.changeTheme(Brightness.dark);
+  //   }
+  //   setThemeinSharedPref(value);
+  // }
 }
