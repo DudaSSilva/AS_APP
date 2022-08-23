814ec363308271097f1e8464195a077547784452
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-23 16:31:52 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/pages/login_page.dart';
-import 'package:untitled/pages/pacote_telaPrincipal.dart';
+import '../pages/login_page.dart';
+import '../pages/pacote_telaPrincipal.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
