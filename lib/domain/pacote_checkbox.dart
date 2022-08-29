13a12d43f941c909b459642b0cadac68ac5d71fb
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-29 11:36:38 -0300

    Update pacote_checkbox.dart

--- lib/domain/pacote_checkbox.dart
+++ lib/domain/pacote_checkbox.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/cupertino.dart';
-import '../models/checkboxModel.dart';
-
-
-final List<CheckBoxModel> itens = [
-  CheckBoxModel(texto: "Domingo"),
-  CheckBoxModel(texto: "Segunda"),
-  CheckBoxModel(texto: "Terça"),
-  CheckBoxModel(texto: "Quarta"),
-  CheckBoxModel(texto: "Quinta"),
-  CheckBoxModel(texto: "Sexta"),
-  CheckBoxModel(texto: "Sábado"),
-];
+// import 'package:flutter/cupertino.dart';
+// import '../models/checkboxModel.dart';
+//
+//
+// final List<CheckBoxModel> itens = [
+//   CheckBoxModel(texto: "Domingo"),
+//   CheckBoxModel(texto: "Segunda"),
+//   CheckBoxModel(texto: "Terça"),
+//   CheckBoxModel(texto: "Quarta"),
+//   CheckBoxModel(texto: "Quinta"),
+//   CheckBoxModel(texto: "Sexta"),
+//   CheckBoxModel(texto: "Sábado"),
+// ];
