ac27a1f1659690095ac638b4d6451ee2ebba2976
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-29 11:35:55 -0300

    Update bdConquistas.dart

--- lib/data/bdConquistas.dart
+++ lib/data/bdConquistas.dart
@@ -1,44 +1,51 @@
-// import '../domain/pacote_turistico.dart';
-//
-// class BD {
-//   static List<PacoteConquistas> lista = [
-//     PacoteConquistas(
-//       titulo: 'Pacote Cancún 2022/2023',
-//       icon: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       text: 'Você realizou todas as tarefas desta semana.',
-//       cor: 0xFFF4D9A9,
-//     ),
-//     PacoteConquistas(
-//       titulo: 'Parabéns!',
-//       icon: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       text: 'Você realizou todas as tarefas desta semana.',
-//       cor: 0xFFF4D9A9,
-//     ),
-//     PacoteConquistas(
-//       titulo: 'Parabéns!',
-//       icon: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       text: 'Você realizou todas as tarefas desta semana.',
-//       cor: 0xFFF4D9A9,
-//     ),
-//     PacoteConquistas(
-//       titulo: 'Parabéns!',
-//       icon: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       text: 'Você realizou todas as tarefas desta semana.',
-//       cor: 0xFFF4D9A9,
-//     ),
-//     PacoteConquistas(
-//       titulo: 'Parabéns!',
-//       icon: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       text: 'Você realizou todas as tarefas desta semana.',
-//       cor: 0xFFF4D9A9,
-//     ),
-//   ];
-//
-//   static Future<List<PacoteConquistas>> getPacoteConquistas() async {
-//     print("ANTES");
-//     await Future.delayed(const Duration(seconds: 10));
-//     print("DEPOIS");
-//     return lista;
-//   }
-//
-// }
+import '../domain/pacote_conquistas.dart';
+
+class BD {
+  static List<PacoteConquistas> lista = [
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+      //cor: 0xFFF4D9A9,
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+    PacoteConquistas(
+      title: 'PARABÉNS!',
+      text: 'Você realizou todas as tarefas desta semana.',
+    ),
+  ];
+
+  static Future<List<PacoteConquistas>> getPacoteConquistas() async {
+    print("ANTES");
+    await Future.delayed(const Duration(seconds: 3));
+    print("DEPOIS");
+    return lista;
+  }
+
+}
