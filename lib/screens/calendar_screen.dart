cd96e59378a69d1208584973dee4fcefeb3a1021
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-30 08:58:53 -0300

    Add files via upload

---
+++ lib/screens/calendar_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+var now = DateTime.now();
+var firstDay = DateTime(now.year, now.month - 7, now.day);
+var lastDay = DateTime(now.year, now.month + 7, now.day);
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat format = CalendarFormat.month;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TableCalendar(
+          focusedDay: now,
+          firstDay: firstDay,
+          lastDay: lastDay,
+          calendarFormat: format,
+          startingDayOfWeek: StartingDayOfWeek.sunday,
+          availableCalendarFormats: const{
+            CalendarFormat.month: 'mês',
+            //CalendarFormat.week: 'semana',
+          },
+
+          headerStyle: const HeaderStyle(
+            leftChevronIcon: Icon(
+              Icons.chevron_left,
+              size: 24,
+              color: Color(0xFFDD2E44),
+            ),
+            rightChevronIcon: Icon(
+              Icons.chevron_right,
+              size: 24,
+              color: Color(0xFFDD2E44),
+            ),
+            titleTextStyle: TextStyle(
+              color: Color(0xFFDD2E44),
+              fontSize: 12,
+            ),
+            titleCentered: true,
+          ),
+        ),
+      ],
+    );
+  }
+}
