3203ddb74c907a49adc04732c97fd1e282ab6450
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-08-30 08:56:59 -0300

    Add files via upload

---
+++ lib/domain/pacote_dica.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+class PacoteDica {
+  final String title;
+  final int color;
+
+  PacoteDica({
+    required this.title,
+    required this.color,
+  });
+}
