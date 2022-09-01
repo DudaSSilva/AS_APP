15f236dc0aee645763ff84b4ae0701ef2b7e0232
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-09-01 15:45:08 -0300

    Update home_rotina.dart

--- lib/pages/home_rotina.dart
+++ lib/pages/home_rotina.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:untitled/pages/pacote_telaPrincipal.dart';
 import '../models/auxiliar_styles.dart';
 
 class HomeRotina extends StatefulWidget {
@@ -17,42 +17,65 @@ class _HomeRotinaState extends State<HomeRotina> {
       backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                            "Vamos, você consegue!", style: Styles.headLineStyle3,
-                        ),
-                        Text(
-                            "Planejamento semanal",style: Styles.headLineStyle,
-                        ),
-                      ],
-                    ),
-
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage(
-                            "images/figuraR.png"
-                          )
-                        )
+          Column(
+            children: [
+              Container(
+                child: IconButton(
+                  icon: const Icon(
+                      Icons.house,
+                    color: Color(0xFFDD2E44),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const TelaPrincipalPage();
+                        },
                       ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Vamos, você consegue!", style: Styles.headLineStyle3,
+                            ),
+                            Text(
+                              "Planejamento semanal",style: Styles.headLineStyle,
+                            ),
+                          ],
+                        ),
+
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: const DecorationImage(
+                                  fit: BoxFit.fitHeight,
+                                  image: AssetImage(
+                                      "images/figuraR.png"
+                                  )
+                              )
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
           )
         ],
       ),
