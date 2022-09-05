fe9f66c7363a8abba10d41c7881ece260da5ec28
Izabelpetuba <103438135+Izabelpetuba@users.noreply.github.com>
2022-09-05 16:13:49 -0300

    Add files via upload

---
+++ lib/domain/pacote_livros.dart
@@ -0,0 +1,20 @@
+
+//import 'package:flutter/cupertino.dart';
+
+class PacoteLivro {
+  final String livro1;
+  final String livro2;
+  final String livro3;
+  final String livro4;
+  final String livro5;
+  final int cor;
+
+  PacoteLivro({
+    required this.livro1,
+    required this.livro2,
+    required this.livro3,
+    required this.livro4,
+    required this.livro5,
+    required this.cor,
+  });
+}
---
+++ lib/domain/pacote_rotina_arg.dart
@@ -0,0 +1,9 @@
+import 'package:untitled/pages/planner/pacote_rotina.dart';
+
+class PacoteRotinaArg {
+  final int cor;
+
+  PacoteRotinaArg({
+    required this.cor,
+  });
+}
