c79978e6610cf36334da57967721ad0102273321
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-10-24 09:30:24 -0300

    Update pacote_habitos_card.dart

--- lib/widget/pacote_habitos_card.dart
+++ lib/widget/pacote_habitos_card.dart
@@ -1,11 +1,14 @@
+import 'package:asapp/screens/calendar_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../domain/habitos.dart';
+
 class CardPacoteHabitos extends StatefulWidget {
-  //final PacoteTarefas pacoteTarefas;
+  final PacoteImages pacoteImages;
 
   const CardPacoteHabitos({
     Key? key,
-    //required this.pacoteTarefas,
+    required this.pacoteImages,
   }) : super(key: key);
 
   @override
@@ -13,7 +16,7 @@ class CardPacoteHabitos extends StatefulWidget {
 }
 
 class _CardPacoteHabitosState extends State<CardPacoteHabitos> {
-  //PacoteTarefas get pacote => widget.pacoteTarefas;
+  PacoteImages get pacote => widget.pacoteImages;
 
   @override
   Widget build(BuildContext context) {
@@ -37,37 +40,25 @@ class _CardPacoteHabitosState extends State<CardPacoteHabitos> {
               children: [
                 //Envolver cada imagem com um card para ficar clicável
 
-                Image.asset(
-                  "images/beleza.png",
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fitHeight,
-                ),
+                BuildImage1(),
                 const SizedBox(height: 20),
-                Image.asset(
-                  "images/casa.png",
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fitHeight,
+                Card(
+                  child: Image.asset(
+                    pacote.images2,
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
               ],
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Image.asset(
-                  "images/estudos.png",
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fitHeight,
-                ),
+
+                BuildImage3(),
                 const SizedBox(height: 20),
-                Image.asset(
-                  "images/saude.png",
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fitHeight,
-                ),
+                BuildImage4(),
               ],
             ),
           ],
@@ -76,317 +67,96 @@ class _CardPacoteHabitosState extends State<CardPacoteHabitos> {
     );
   }
 
-  /*
-  SingleChildScrollView buildListView() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  color: const Color(0xFFFFF6E5),
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Tarefas',
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        'S',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        'T',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        'Q',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        'Q',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        'S',
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Responder simulado',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Mapas mentais',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Lista exercícios',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Resumos',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Vídeo aulas',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 32),
-                Container(
-                  color: const Color(0xFFFFF6E5),
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Tarefas',
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'S',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'T',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'Q',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'Q',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Text(
-                        'S',
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Responder simulado',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Mapas mentais',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Lista exercícios',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Resumos',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Vídeo aulas',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                          const Icon(Icons.circle),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+  BuildImage1() {
+    return InkWell(
+        onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return CalendarScreen();
+          },
+        ),
+      );
+    },
+    child: Card(
+      child: Image.asset(
+        pacote.images,
+        width: 200,
+        height: 200,
+        fit: BoxFit.fitHeight,
+      ),
+    ),
+    );
+  }
+
+  BuildImage2() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CalendarScreen();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: Image.asset(
+          pacote.images2,
+          width: 200,
+          height: 200,
+          fit: BoxFit.fitHeight,
+        ),
       ),
     );
   }
-  */
+
+  BuildImage3() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CalendarScreen();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: Image.asset(
+          pacote.images3,
+          width: 200,
+          height: 200,
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
+  }
+
+  BuildImage4() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CalendarScreen();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: Image.asset(
+          pacote.images4,
+          width: 200,
+          height: 200,
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
+  }
+
 }
