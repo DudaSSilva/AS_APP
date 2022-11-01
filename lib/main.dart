67f311e1d3bf8f086036e7d7ecbe86148e67aa20
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-11-01 07:36:14 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,33 @@
-import 'package:asapp/pages/planner/home_rotina.dart';
-import 'package:asapp/pages/planner/livros.dart';
-import 'package:asapp/widget/add_book_bar.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../screens/splash_screen.dart';
-import 'package:asapp/services/notification_services.dart';
-import 'pages/home_page.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:developer';
 
 FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    WidgetsFlutterBinding.ensureInitialized();
-    AndroidInitializationSettings androidSettings = AndroidInitializationSettings("@mipmap/ic_launcher");
+  AndroidInitializationSettings androidSettings = AndroidInitializationSettings("@mipmap/ic_launcher");
 
-    DarwinInitializationSettings iosSettings = DarwinInitializationSettings(
-      requestSoundPermission: true,
-      requestBadgePermission: true,
-      requestCriticalPermission: true,
-      requestAlertPermission: true,
-    );
+  DarwinInitializationSettings iosSettings = DarwinInitializationSettings(
+    requestSoundPermission: true,
+    requestBadgePermission: true,
+    requestCriticalPermission: true,
+    requestAlertPermission: true,
+  );
 
-    InitializationSettings initializationSettings = InitializationSettings(
-      android: androidSettings,
-      iOS: iosSettings,
-    );
+  InitializationSettings initializationSettings = InitializationSettings(
+    android: androidSettings,
+    iOS: iosSettings,
+  );
 
-    bool? initialized = await notificationsPlugin.initialize(initializationSettings);
+  bool? initialized = await notificationsPlugin.initialize(initializationSettings);
 
-    log("Notifications $initialized");
+  log("Notifications $initialized");
 
   runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: Splash()
-      // Splash()
-    ),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: Splash()),
   );
 }
-
-
