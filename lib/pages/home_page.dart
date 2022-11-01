f14aa0c0cdb9532380ac88c8cf90bef85045213e
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-11-01 07:35:37 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../main.dart';
 import 'login/login_page.dart';
-import '../pages/pacote_telaPrincipal.dart';
-import 'package:asapp/main.dart';
-import 'package:asapp/services/notification_services.dart';
-import 'package:asapp/services/themes_services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import '../../data/dataSimulatory/bdLivros.dart';
-import '../../domain/pacote_livros.dart';
-import '../../widget/pacote_livros_card.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,40 +13,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void showNotification() async {
-    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
-      "as-app",
-      "ASApp",
-      priority: Priority.max,
-      importance: Importance.max,
-    );
-
-    DarwinNotificationDetails iosDetails = DarwinNotificationDetails(
-      presentAlert: true,
-      presentBadge: true,
-      presentSound: true,
-    );
-
-    NotificationDetails notiDetails = NotificationDetails(
-      android: androidDetails,
-      iOS: iosDetails,
-    );
-
-    await notificationsPlugin.show(
-        0,
-        "Notificação teste",
-        "Seja bem vindo, queridinha!",
-        notiDetails
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: showNotification,
-        child: Icon(Icons.notification_add),
-      ),
       backgroundColor: const Color(0xFFDD2E44),
       body: Padding(
         padding: const EdgeInsets.all(16),
