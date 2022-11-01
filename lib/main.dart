a8403f59abb7807a32f17971c231f1e4a14674ac
DudaSSilva <103438137+DudaSSilva@users.noreply.github.com>
2022-11-01 08:59:01 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
-import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
+import 'package:asapp/screens/animation.dart';
 import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
 
-import 'screens/splash_screen.dart';
-
-//FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
-
-void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // WidgetsFlutterBinding.ensureInitialized();
-  // AndroidInitializationSettings androidSettings = AndroidInitializationSettings("@mipmap/ic_launcher");
-  //
-  // DarwinInitializationSettings iosSettings = DarwinInitializationSettings(
-  //   requestSoundPermission: true,
-  //   requestBadgePermission: true,
-  //   requestCriticalPermission: true,
-  //   requestAlertPermission: true,
-  // );
-  //
-  // InitializationSettings initializationSettings = InitializationSettings(
-  //   android: androidSettings,
-  //   iOS: iosSettings,
-  // );
-  //
-  // bool? initialized = await notificationsPlugin.initialize(initializationSettings);
-  //
-  // log("Notifications $initialized");
-
+void main() {
   runApp(
     const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Splash()
-      // Splash()
+        home: AnimationPage(page: Splash())
     ),
   );
 }
