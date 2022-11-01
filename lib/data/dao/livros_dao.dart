cf52b414eddb6a1e55c3b58b74cb8c7238a8faa5
jumelo-n <jenm1@aluno.ifal.edu.br>
2022-11-01 07:33:54 -0300

    Notificações

--- lib/data/dao/livros_dao.dart
+++ lib/data/dao/livros_dao.dart
@@ -1,27 +1,27 @@
-import 'package:asapp/domain/pacote_livros.dart';
-import 'package:sqflite/sqflite.dart';
-import '../../domain/pacote_conteudos.dart';
-import '../../domain/pacote_tarefas.dart';
-import '../helper/tarefas_db_helper.dart';
-
-class LivrosDao {
-
-  Future<List<PacoteLivro>> listarLivrose() async {
-    DBHelper dbHelper = DBHelper();
-    Database db = await dbHelper.initDB();
-
-    String sql = 'SELECT * FROM LIVROS;';
-    final result = await db.rawQuery(sql);
-
-
-    List<PacoteConteudos> lista = <PacoteConteudos>[];
-    for(var json in result){
-      print(json);
-      PacoteConteudos conteudos = PacoteConteudos.fromJson(json);
-      lista.add(conteudos);
-    }
-
-    return lista;
-  }
-
-}
+// import 'package:asapp/domain/pacote_livros.dart';
+// import 'package:sqflite/sqflite.dart';
+// import '../../domain/pacote_conteudos.dart';
+// import '../../domain/pacote_tarefas.dart';
+// import '../helper/tarefas_db_helper.dart';
+//
+// class LivrosDao {
+//
+//   Future<List<PacoteLivro>> listarLivrose() async {
+//     DBHelper dbHelper = DBHelper();
+//     Database db = await dbHelper.initDB();
+//
+//     String sql = 'SELECT * FROM LIVROS;';
+//     final result = await db.rawQuery(sql);
+//
+//
+//     List<PacoteLivro> lista = <PacoteLivro>[];
+//     for(var json in result){
+//       print(json);
+//       PacoteLivro conteudos = PacoteLivro.fromJson(json);
+//       lista.add(conteudos);
+//     }
+//
+//     return lista;
+//   }
+//
+// }
